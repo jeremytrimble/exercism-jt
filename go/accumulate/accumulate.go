c784bc13ed8c2f5e6e6c178e8322d7aa27294b8d
Jeremy Trimble <jeremy.trimble@gmail.com>
2017-08-29 15:56:23 -0400

    accumulate solution

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -2,4 +2,14 @@ package accumulate
 
 const testVersion = 1
 
-func Accumulate([]string, func(string) string) []string
+func Accumulate( in []string, fun func(string) string) (out []string)  {
+
+    out = make( []string, len(in) )
+
+    for i := 0; i < len(in); i++ {
+        out[i] = fun(in[i])
+    }
+
+    return
+
+}
