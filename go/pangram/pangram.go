3c2cdc8e93f18095866958af778a98b8f6afa811
Jeremy Trimble <jeremy.trimble@gmail.com>
2017-08-31 14:18:39 -0400

    pangram: Done.  With gofmt.

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,35 @@
 package pangram
 
+import "strings"
+
 const testVersion = 2
 
-func IsPangram(string) bool
+type BitVec struct {
+	bits uint32
+}
+
+func NewBitvec() BitVec {
+	return BitVec{bits: 0}
+}
+
+func (bv *BitVec) SetRune(r rune) {
+	bv.Set(uint(strings.ToUpper(string(r))[0]) - uint('A'))
+}
+
+func (bv *BitVec) Set(bit uint) {
+	bv.bits |= (1 << bit)
+}
+
+func (bv *BitVec) IsComplete() bool {
+	return bv.bits == 0x3FFFFFF
+}
+
+func IsPangram(in string) bool {
+	var bv BitVec
+
+	for _, r := range in {
+		bv.SetRune(r)
+	}
+
+	return bv.IsComplete()
+}
