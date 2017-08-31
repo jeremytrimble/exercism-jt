c5edb7c955a6d1cafe20b3598219e51aebeab029
Jeremy Trimble <jeremy.trimble@gmail.com>
2017-08-31 15:15:48 -0400

    bob.go:  Works.  That was annoying.

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,49 +1,65 @@
 package bob
 
-import "fmt"
+//import "fmt"
+import "strings"
+import "unicode"
 
 const testVersion = 3
 
-func any_words_all_caps( runes []rune ) (bool,bool) {
+func count_yelling( words []string ) (int, int) {
 
-    any_all_caps  := false
-    word_all_caps := true
-    total_caps    := true
+    total_words := 0
+    total_yells := 0
 
-    for _, ru := range runes {
-        up := int(ru)
+    for _, word := range words {
 
-        if !(int('A') <= up && up <= int('Z')) {
-            word_all_caps = false
-            total_caps    = false
-        }
+        is_word     := false
+        is_all_caps := true
 
-        if ru == ' ' {
-            if word_all_caps {
-                any_all_caps = true
+        for _, ru := range word {
+
+            if unicode.IsLetter(ru) {
+                is_word = true
+                if !unicode.IsUpper(ru) {
+                    is_all_caps = false
+                }
             }
-            word_all_caps = true
+
         }
+
+        if is_word {
+            total_words++
+        }
+
+        if is_word && is_all_caps {
+            total_yells++
+        }
+
     }
 
-    return any_all_caps, total_caps
+    return total_yells, total_words
 }
 
 // func Hey simulates interaction with a stereotyped millenial.
 func Hey( in string ) string {
 
-    fmt.Println("in: ", in)
+    //fmt.Println("in: ", in)
 
-    runes := []rune(in)
+    words := strings.Fields(in)
 
-    any_all_caps, total_caps  := any_words_all_caps( runes )
-    ends_with_exc := runes[ len(runes)-1 ] == '!'
+    if len(words) == 0 {
+        return "Fine. Be that way!"
+    }
 
-    fmt.Println("any_all_caps", any_all_caps, "total_caps", total_caps)
+    total_yells, total_words := count_yelling( words )
 
-    ends_with_que := runes[ len(runes)-1 ] == '?'
-    
-    if (any_all_caps && ends_with_exc) || total_caps {
+    ends_with_que := strings.HasSuffix(words[len(words)-1], "?")
+
+    mostly_yelling := total_yells > 0 && float32(total_yells) >= (float32(total_words)/float32(2.0))
+
+    //fmt.Println("total_yells", total_yells, "total_words", total_words)
+
+    if mostly_yelling {
         return "Whoa, chill out!"
     }
 
